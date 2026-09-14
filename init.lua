2c065c0d3679bc653d8cf49e663519574b3fdd30
anonymous <120080805+selinuntius40@users.noreply.github.com>
2026-09-14 23:54:49 +0900

    updating installations and content

--- init.lua
+++ init.lua
@@ -249,7 +249,7 @@ do
   vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function() vim.hl.on_yank() end,
+    callback = function() vim.hl.hl_op() end,
   })
 end
 
